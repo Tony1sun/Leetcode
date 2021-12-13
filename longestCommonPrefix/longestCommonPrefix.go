5fc382e9de14dd2d5852f8e0ce121ea93f417b96
Tony1sun <jacktony822@gmail.com>
2021-12-13 23:28:57 +0800

    add

---
+++ longestCommonPrefix/longestCommonPrefix.go
@@ -0,0 +1,42 @@
+package Leetcode
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 最长公共前缀
+// 编写一个函数来查找字符串数组中的最长公共前缀。
+
+// 如果不存在公共前缀，返回空字符串 ""。
+// 示例 1：
+// 输入：strs = ["flower","flow","flight"]
+// 输出："fl"
+// 示例 2：
+// 输入：strs = ["dog","racecar","car"]
+// 输出：""
+// 解释：输入不存在公共前缀。
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	result := strs[0]
+	for _, value := range strs {
+		// 如果result在value出现
+		for strings.Index(value, result) != 0 {
+			// 消减result，直到result和value匹配
+			result = result[0 : len(result)-1]
+			if len(result) == 0 {
+				return ""
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	strs := []string{"flower", "flow", "flight"}
+	fmt.Println(longestCommonPrefix(strs))
+}
---
+++ longestCommonPrefix/longestCommonPrefix_test.go
@@ -0,0 +1,12 @@
+package Leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_longestCommonPrefixtlo(t *testing.T) {
+	strs := []string{"flower", "flow", "flight"}
+	fmt.Println(longestCommonPrefix(strs))
+
+}
