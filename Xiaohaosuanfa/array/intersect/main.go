ab2a87e3323a915b14bdc50c58b49636b3592f3f
Tony1sun <jacktony822@gmail.com>
2022-03-10 22:35:22 +0800

    add

---
+++ Xiaohaosuanfa/array/intersect/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func intersect(nums1 []int, nums2 []int) []int {
+	m0 := map[int]int{}
+	for _, v := range nums1 {
+		// 遍历nums1，初始化map
+		m0[v] += 1
+	}
+	k := 0
+	for _, v := range nums2 {
+		// 如果元素相同， 将其存入nums2中， 并将出现次数减1
+		if m0[v] > 0 {
+			m0[v] -= 1
+			nums2[k] = v
+			k++
+		}
+	}
+	return nums2[0:k]
+
+}
+
+func main() {
+	nums1 := []int{1, 2, 2, 2, 1}
+	nums2 := []int{2, 3, 2}
+	fmt.Println(intersect(nums1, nums2))
+}
