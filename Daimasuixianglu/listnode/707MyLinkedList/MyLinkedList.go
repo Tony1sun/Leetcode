9931ea1678d5fc6f15e1e0c00fad6d1e9a335ce5
Tony1sun <jacktony822@gmail.com>
2022-04-09 13:18:11 +0800

    add

--- Daimasuixianglu/listnode/707MyLinkedList/MyLinkedList.go
+++ Daimasuixianglu/listnode/707MyLinkedList/MyLinkedList.go
@@ -14,6 +14,7 @@ func Constructor() MyLinkedList {
 	return MyLinkedList{}
 }
 
+// get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
 func (this *MyLinkedList) Get(index int) int {
 	head := this.head
 	for i := 0; i < index && head != nil; i++ {
@@ -26,3 +27,15 @@ func (this *MyLinkedList) Get(index int) int {
 	}
 }
 
+// addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+func (this *MyLinkedList) AddAtHead(val int) {
+	node := &Node{Val: val}
+	node.Next = this.head
+	if this.head != nil {
+		this.head.Prev = node
+	}
+	this.head = node
+}
+
+// addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+func (this *MyLinkedList) AddAtTail(val int)
